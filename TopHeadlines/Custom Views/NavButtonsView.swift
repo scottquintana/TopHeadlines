56892e52fd7c24cdfd6998b19dd77cc0a0f8bda9
Scott Quintana <mail@scottquintana.com>
2020-10-26 21:42:55 -0500

    Initial commit

---
+++ TopHeadlines/Custom Views/NavButtonsView.swift
@@ -0,0 +1,13 @@
+//
+//  NavButtons.swift
+//  TopHeadlines
+//
+//  Created by Scott Quintana on 10/26/20.
+//
+
+import UIKit
+
+class NavButtons: UIView {
+
+
+}
---
+++ TopHeadlines/Custom Views/SwiperView.swift
@@ -0,0 +1,14 @@
+//
+//  SwipeTown.swift
+//  TopHeadlines
+//
+//  Created by Scott Quintana on 10/26/20.
+//
+
+import UIKit
+
+class SwipeTown: UIView {
+
+
+
+}
